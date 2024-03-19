b6e6f83d94abbfaaa2835fd9030e8e5fb6e8d246
ATiKA0 <99249171+ATiKA0@users.noreply.github.com>
2024-03-19 16:13:01 +0100

    add services files and repos for all data

---
+++ food_order_backend/src/main/java/com/gluck/food_order/repository/IngredientRepository.java
@@ -0,0 +1,12 @@
+package com.gluck.food_order.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gluck.food_order.model.Ingredient;
+
+public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
+
+    Optional<Ingredient> findByName(String name);
+}
---
+++ food_order_backend/src/main/java/com/gluck/food_order/service/IngredientService.java
@@ -0,0 +1,8 @@
+package com.gluck.food_order.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class IngredientService {
+
+}
